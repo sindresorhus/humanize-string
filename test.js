b5593e2396621308fb4abc2582404334c589e22d
stoeffel <schtoeffel@gmail.com>
2015-01-20 14:20:22 +0100

    fixes humanize strings with a prefixed dash
    refs epeli/underscore.string/#391

--- test.js
+++ test.js
@@ -9,6 +9,7 @@ test(function (t) {
 	t.assert(hs('UnicornsAndRainbows') === 'Unicorns and rainbows');
 	t.assert(hs('unicornsAndRainbows') === 'Unicorns and rainbows');
 	t.assert(hs('unicorns_and_rainbows') === 'Unicorns and rainbows');
+	t.assert(hs('-UnicornsAndRainbows') === 'Unicorns and rainbows');
 	t.assert(hs('  unicorns  and  rainbows  ') === 'Unicorns and rainbows');
 	t.assert(hs('unicorns_and-Rainbows_andPonies  ') === 'Unicorns and rainbows and ponies');
 	t.end();
