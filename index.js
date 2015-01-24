59618d7967719c7414743c4a591a7893ab9ffaa3
Sindre Sorhus <sindresorhus@gmail.com>
2015-01-24 19:13:26 +0800

    modularize
    not going to do it with the other lines as they're straight forward and not worth the extra dependency size.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 'use strict';
+var decamelize = require('decamelize');
+
 module.exports = function (str) {
 	if (typeof str !== 'string') {
 		throw new TypeError('Expected a string');
 	}
 
-	str = str.replace(/[A-Z]/g, function (m) {
-	  return ' ' + m.toLowerCase();
-	});
-
+	str = decamelize(str);
 	str = str.toLowerCase().replace(/[_-]+/g, ' ').replace(/\s{2,}/g, ' ').trim();
 	str = str.charAt(0).toUpperCase() + str.slice(1);
 
